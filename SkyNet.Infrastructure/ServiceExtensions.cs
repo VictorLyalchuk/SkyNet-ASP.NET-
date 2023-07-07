11226e5c784d9db20806527d3eb78ac6c0fca711
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2023-07-07 18:24:17 +0300

    Add project files.

---
+++ SkyNet.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SkyNet.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyNet.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbContext(this IServiceCollection service, string connectionString)
+        {
+            service.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+    }
+}
